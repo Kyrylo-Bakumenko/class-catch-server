b36c79bf30985d8c5e5af38c4b2f9c2d1db8346f
Kyrylo Bakumenko <kyrylobakumenko@Kyrylos-MacBook-Pro.local>
2025-01-28 19:10:26 -0500

    updated auto scrape scripts for Railway

--- refresh_proxies.sh
+++ refresh_proxies.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Define the paths
-VENV_PYTHON="/Users/kyrylobakumenko/miniconda3/envs/class-catch-env/bin/python"
-PROJECT_DIR="/Users/kyrylobakumenko/vscode/class-catch/backend/"
-LOG_FILE="/Users/kyrylobakumenko/vscode/class-catch/backend/logs/refresh_proxies.log"
+# # Define the paths
+# VENV_PYTHON="/Users/kyrylobakumenko/miniconda3/envs/class-catch-env/bin/python"
+# PROJECT_DIR="/Users/kyrylobakumenko/vscode/class-catch/backend/"
+# LOG_FILE="/Users/kyrylobakumenko/vscode/class-catch/backend/logs/refresh_proxies.log"
 
-# Navigate to the project directory
-cd "$PROJECT_DIR"
+# # Navigate to the project directory
+# cd "$PROJECT_DIR"
 
-# Run the proxy refreshing command
-"$VENV_PYTHON" manage.py refresh_proxies >> "$LOG_FILE" 2>&1
+# # Run the proxy refreshing command
+# "$VENV_PYTHON" manage.py refresh_proxies >> "$LOG_FILE" 2>&1
+
+### DEPLOYED VERSION ###
+python manage.py refresh_proxies >> logs/refresh_proxies.log 2>&1
--- run_scrape_classes.sh
+++ run_scrape_classes.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Define the paths
-VENV_PYTHON="/Users/kyrylobakumenko/miniconda3/envs/class-catch-env/bin/python"
-PROJECT_DIR="/Users/kyrylobakumenko/vscode/class-catch/backend/"
-LOG_FILE="/Users/kyrylobakumenko/vscode/class-catch/backend/logs/scrape_classes.log"
+# # Define the paths
+# VENV_PYTHON="/Users/kyrylobakumenko/miniconda3/envs/class-catch-env/bin/python"
+# PROJECT_DIR="/Users/kyrylobakumenko/vscode/class-catch/backend/"
+# LOG_FILE="/Users/kyrylobakumenko/vscode/class-catch/backend/logs/scrape_classes.log"
 
-# Navigate to the project directory
-cd "$PROJECT_DIR"
+# # Navigate to the project directory
+# cd "$PROJECT_DIR"
 
-# Run the scraping command
-"$VENV_PYTHON" manage.py scrape_classes >> "$LOG_FILE" 2>&1
+# # Run the scraping command
+# "$VENV_PYTHON" manage.py scrape_classes >> "$LOG_FILE" 2>&1
+
+### DEPLOYED VERSION ###
+python manage.py scrape_classes >> logs/scrape_classes.log 2>&1
